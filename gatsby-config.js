28c371755fc3cf6995b5f1edce6a075a8ae66e20
jeffwolff <jeff.wolff@cukerinteractive.com>
2019-04-11 11:03:19 -0700

    analytics and work post nav flex

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-486826-17`,
       },
     },
     `gatsby-plugin-styled-components`,
