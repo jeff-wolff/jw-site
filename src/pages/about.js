1b94bb08ff2394b92fe8ddb5a1edc0f2d2c3fe89
jeffwolff <jeff.wolff@cukerinteractive.com>
2019-04-11 15:34:52 -0700

    minor tweaks

--- src/pages/about.js
+++ src/pages/about.js
@@ -89,7 +89,7 @@ class About extends React.Component {
               <h2>About Jeff Wolff</h2>
               <p>I have been designing and developing digital experiences for brands, agencies, and start-ups for over 10 years. A &ldquo;creative developer&rdquo; at heart, my passion is creating elegant, engaging, and easy-to-use websites through well-crafted code.</p>
               <p>Some clients I have had the pleasure to work with are <Link to="/ripcurl/">Rip Curl</Link>, <Link to="/vans/">Vans</Link>, <Link to="/gomacro/">GoMacro</Link>, <Link to="/sunbelt-rentals/">Sunbelt Rentals</Link>, and <Link to="/cuker/">Cuker Agency</Link>.</p>
-              <p>Currently listening to <img src={EqualizerGif} style={{ width: '10px' }} /> <a href={songURL} target="_blank">{songArtist} - {songTitle}</a>.<br /><a href="https://www.last.fm/user/jeffwolff" target="_blank"><small>powered by last.fm api</small></a></p>
+              <p>Currently listening to:<br /><img src={EqualizerGif} style={{ width: '10px' }} /> <a href={songURL} target="_blank">{songArtist} - {songTitle}</a><br /><a href="https://www.last.fm/user/jeffwolff" target="_blank"><small>powered by last.fm api</small></a></p>
               <div className="window">
                 <div className="window-title"><div className="content">Navigation</div></div>
                 <div className="window-content">
