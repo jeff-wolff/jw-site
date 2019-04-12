77e89cdc4f2fbb9b28b2f8e7b441315c1abda5e5
jeff <xhilacom@gmail.com>
2019-04-11 20:17:27 -0700

    fix config syntax2

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-sitemap`
+      resolve: `gatsby-plugin-sitemap`,
       options: {
         exclude: ["/react/"],
       }
