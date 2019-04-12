213fe6fb6c4c77060359538cff2fa365de89edbb
jeff <xhilacom@gmail.com>
2019-04-11 20:27:47 -0700

    test exclude2

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        exclude: [`/react/`],
+        exclude: [`https://www.jeffwolff.net/react/`],
       }
     },
     `gatsby-plugin-react-helmet`,
